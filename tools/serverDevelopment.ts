1258f5f055c79ecbf5b3e34ee50be1c10f298404
Guilherme Conti Teixeira <guibasconti@gmail.com>
2021-07-30 01:52:49 -0400

    Update packages

--- tools/serverDevelopment.ts
+++ tools/serverDevelopment.ts
@@ -1,10 +1,8 @@
 import express from 'express';
-import morgan from 'morgan';
 import router from '../server/core/router';
 
 const developmentApp = express();
 
 developmentApp.use('/', router);
-developmentApp.use(morgan('tiny'));
 
 module.exports = developmentApp;
